710de0d96c20ef06dcc946e4e7dd0a473a0ced16
Ephra <ephygtz@gmail.com>
2024-02-15 23:45:08 +0300

    bug: hack define schema before assigning

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 //define schema
 //hack to pass fcc validation
 const SchemaHack = mongoose.Schema;
-let personSchema = SchemaHack({
+let personSchema = new SchemaHack({
   name : {
     type : String,
     required : true,
